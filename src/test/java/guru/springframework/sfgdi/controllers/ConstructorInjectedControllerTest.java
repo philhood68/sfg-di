e386bf79d6eee6992950bc5afa4c3304505758f6
philhood68 <phil.hood@outlook.com>
2020-06-07 14:53:46 +0100

    Controllers

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+
+public class ConstructorInjectedControllerTest 
+{
+	ConstructorInjectedController controller;
+	
+	@BeforeEach
+	void setUp()
+	{
+		controller = new ConstructorInjectedController( new GreetingServiceImpl() );
+	}
+	
+	@Test
+	void getGreeting()
+	{
+		System.out.println( controller.getGreeting() );
+	}
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+
+class PropertyInjectedControllerTest 
+{
+	PropertyInjectedController controller;
+	
+	@BeforeEach
+	void setUp()
+	{
+		controller = new PropertyInjectedController();
+		
+		controller.greetingService = new GreetingServiceImpl();
+	}
+	
+	@Test
+	void getGreeting()
+	{
+		System.out.println( controller.getGreeting() );
+	}
+}
