24025bf3098e29443270af9cc92960d780bcd704
philhood68 <phil.hood@outlook.com>
2020-06-14 15:32:58 +0100

    Update CatPatService.java

--- src/main/java/guru/springframework/sfgdi/services/CatPatService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPatService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Profile( "cat" )
-@Service("PetService")
+@Service
 public class CatPatService implements PetService 
 {
 	@Override
